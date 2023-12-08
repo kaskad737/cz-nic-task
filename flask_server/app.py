b2c1dff623c2ed7cc5d25fa7cfab230568358125
Apti <Archakov.Apti@email.cz>
2023-12-08 10:38:22 +0000

    fixed the bug in flask app

--- flask_server/app.py
+++ flask_server/app.py
@@ -15,46 +15,51 @@ MOCK_DATABASE = {
 
 @app.route('/file/<uuid>/stat/', methods=['GET'])
 def file_metadata(uuid):
-    data_from_db = MOCK_DATABASE[uuid]
-    file_path = data_from_db['file_path'] + data_from_db['file_name']
-    file_name = os.path.basename(file_path)
+    data_from_db = MOCK_DATABASE.get(uuid)
+    if data_from_db:
+        file_path = data_from_db['file_path'] + data_from_db['file_name']
+        file_name = os.path.basename(file_path)
+        if not os.path.isfile(file_path):
+            return 'File not found. Error code 404', 404
+        else:
+            create_datetime = os.path.getmtime(file_path)
+            size = os.path.getsize(file_path)
+            mimetype = mimetypes.guess_type(file_path)[0]
+            name = os.path.splitext(file_name)[0]
 
-    if not os.path.isfile(file_path):
-        return 'File not found. Error code 404', 404
+            file_metadata = {
+                'create_datetime': create_datetime,
+                'size': size,
+                'mimetype': mimetype,
+                'name': name,
+            }
+
+            return jsonify(file_metadata)
     else:
-        create_datetime = os.path.getmtime(file_path)
-        size = os.path.getsize(file_path)
-        mimetype = mimetypes.guess_type(file_path)[0]
-        name = os.path.splitext(file_name)[0]
-
-        file_metadata = {
-            'create_datetime': create_datetime,
-            'size': size,
-            'mimetype': mimetype,
-            'name': name,
-        }
-
-        return jsonify(file_metadata)
-
+        return 'File not found. Error code 404', 404
+    
 @app.route('/file/<uuid>/read/', methods=['GET'])
 def read_file(uuid):
-    data_from_db = MOCK_DATABASE[uuid]
-    file_path = data_from_db['file_path'] + data_from_db['file_name']
-    file_name = os.path.basename(file_path)
+    data_from_db = MOCK_DATABASE.get(uuid)
+    if data_from_db:
+        file_path = data_from_db['file_path'] + data_from_db['file_name']
+        file_name = os.path.basename(file_path)
 
-    if not os.path.isfile(file_path):
+        if not os.path.isfile(file_path):
+            return 'File not found. Error code 404', 404
+
+        mimetype = mimetypes.guess_type(file_path)
+
+        with open(file_path, 'r') as f:
+            file_content = f.read()
+
+        resp = make_response(file_content, 200)
+        resp.headers['Content-Disposition'] = file_name
+        resp.headers['Content-Type'] = mimetype[0]
+
+        return resp
+    else:
         return 'File not found. Error code 404', 404
-
-    mimetype = mimetypes.guess_type(file_path)
-
-    with open(file_path, 'r') as f:
-        file_content = f.read()
-
-    resp = make_response(file_content, 200)
-    resp.headers['Content-Disposition'] = file_name
-    resp.headers['Content-Type'] = mimetype[0]
-
-    return resp
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
