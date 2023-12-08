72b1deda2294a8a2c1bed968bb7ee768b348732f
Apti <Archakov.Apti@email.cz>
2023-12-08 20:49:25 +0000

    add test to flask  on error case

--- flask_server/test/test_flask_app.py
+++ flask_server/test/test_flask_app.py
@@ -18,3 +18,14 @@ def test_read_file_endpoint(client):
     response = client.get('/file/1/read/')
     assert response.status_code == 200
     assert response.text[:26] == 'Lorem ipsum dolor sit amet'
+
+def test_file_metadata_endpoint_file_not_found(client):
+    response = client.get('/file/5/stat/')
+    assert response.status_code == 404
+    assert response.text == 'File not found. Error code 404'
+
+def test_read_file_endpoint_file_not_found(client):
+    response = client.get('/file/5/read/')
+    assert response.status_code == 404
+    print(response.text)
+    assert response.text == 'File not found. Error code 404'
